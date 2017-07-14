01b68328ae770094a8031f51f9a30b5a4dc019fe
michaeljohnorlando <michael.john.orlando@gmail.com>
2017-07-14 18:56:54 -0400

    simple twiter like posting

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  #basic starting 
-  root 'application#hello'
+  # starting point of web page... root
+  root 'users#index'
 end
